ac54c6c04e95151b24a9aee7ce20c3d105ce296d
Arpita Saha <arpita.sahaa@gmail.com>
2021-06-16 10:08:20 -0400

    added hooks api-call

--- src/Container/App.js
+++ src/Container/App.js
@@ -1,18 +1,27 @@
 import "./App.css";
+import { useState } from "react";
 import Display5DaysForecast from "../components/Display5DaysForecast";
 import DisplayCitiesName from "../components/DisplayCitiesName";
 import DisplayCityForcast from "../components/DisplayCityForcast";
 import SearchBar from "../components/SearchBar";
 
+
 function App() {
+  const [textHandler, setTextHandler] = useState("");
+  const [weatherForecast, setWeatherForecast] = useState({});
+
   return (
     <div className="App">
       <h1 className="header">Weather Dashboard</h1>
-      <section className = "xyz">
-        <SearchBar />
+      <section className="xyz">
+        <SearchBar
+          setTextHandler={setTextHandler}
+          setWeatherForecast={setWeatherForecast}
+          textHandler={textHandler}
+        />
         <DisplayCitiesName />
       </section>
-        <DisplayCityForcast />
+      <DisplayCityForcast />
       <Display5DaysForecast />
     </div>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import axios from "axios";
 
-function SearchBar() {
+function SearchBar({setTextHandler,setWeatherForecast,textHandler}) {
+
+    const setText = (e) => {
+        setTextHandler(e.target.value);
+    }
+
+    const getWeatherForecast = () =>{
+        const APIKey = "2be1f85ce6c480a430250905886eb88e";
+        const URL = `https://api.openweathermap.org/data/2.5/weather?q=${textHandler}&appid=${APIKey}`
+        
+        axios.get(URL).then(res=>{
+          setWeatherForecast(res.data)
+          console.log(res.data)
+        })
+      }
+
     return (
         <div className="searcBar">
             <h3>Search for city</h3>
-            <input type="text"/>
-            <button type="button">search</button>
+            <input onChange={setText} type="text"/>
+            <button onClick = {getWeatherForecast} type="button">search</button>
         </div>
     )
 }
